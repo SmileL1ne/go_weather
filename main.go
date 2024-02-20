3ccfe10c665e45cf3281b648112cda996c95698c
Sarvarov Mustafa <sarvarov.mustafa@gmail.com>
2024-02-20 16:12:48 +0600

    feat: add initial setup

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type application struct {
+}
 
 func main() {
-	fmt.Println("hello nah")
+	app := application{}
+
+	router := httprouter.New()
+	router.HandlerFunc(http.MethodGet, "/weather", app.weather)
+	router.HandlerFunc(http.MethodPost, "/weather", app.weatherPost)
+}
+
+func (app *application) weather(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func (app *application) weatherPost(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func OpenConnection(uri string) (*mongo.Client, error) {
+	options := options.Client().ApplyURI(uri)
+
+	client, err := mongo.Connect(context.Background(), options)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := client.Ping(context.Background(), nil); err != nil {
+		if err := client.Disconnect(context.Background()); err != nil {
+			panic(err)
+		}
+		return nil, err
+	}
+
+	return client, err
 }
